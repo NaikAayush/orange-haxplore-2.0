9efbae8ef20934cfaf5fda9b93967fe0f6da5d22
NaikAayush <ayush@pesu.pes.edu>
2021-03-28 02:33:08 +0530

    added firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,41 @@
+var express = require("express");
+var axios = require("axios");
+
 const functions = require("firebase-functions");
-import express from 'express';
-import axios from 'axios';
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
 
 const app = express();
+const cors = require("cors");
+app.use(cors({ origin: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PATCH,DELETE,OPTIONS,PUT"
+  );
+  next();
+});
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-const blockServer = 'http://34.87.52.243:3000';
-const entityName = 'Test';
+const blockServer = "http://34.87.52.243:3000";
+const entityName = "Test";
 
-app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+app.get("/", (req, res) => {
+  return res.send("Received a GET HTTP method");
 });
 
 async function getCrop(cropId) {
-  return axios.get(`${blockServer}/api/${entityName}/${cropId}`)
+  return axios
+    .get(`${blockServer}/api/${entityName}/${cropId}`)
     .then((resp) => {
       return resp.data;
     })
@@ -28,24 +48,24 @@ async function getCrop(cropId) {
     });
 }
 
-app.get('/get/:cropId', async (req, res) => {
+app.get("/get/:cropId", async (req, res) => {
   let [status, data] = await getCrop(req.params.cropId);
   console.log(status, data);
   data = JSON.parse(data);
   console.log(status, data);
   if (!status) res.status(data.code);
-  return res.send({"success": status, "data": data});
+  return res.send({ success: status, data: data });
 });
 
-
-app.post('/create/:cropId', async (req, res) => {
+app.post("/create/:cropId", async (req, res) => {
   let data = {
-    "$class": "org.orange.organicchain.Test",
-    "cropId": req.params.cropId,
-    "data": JSON.stringify(req.body)
+    $class: "org.orange.organicchain.Test",
+    cropId: req.params.cropId,
+    data: JSON.stringify(req.body),
   };
   console.log(data);
-  let [status, ret] = await axios.post(`${blockServer}/api/${entityName}`, data)
+  let [status, ret] = await axios
+    .post(`${blockServer}/api/${entityName}`, data)
     .then((resp) => {
       return [true, resp.data];
     })
@@ -55,24 +75,24 @@ app.post('/create/:cropId', async (req, res) => {
     });
   console.log(status, ret);
 
-  return res.send({"success": status, "data": ret});
+  return res.send({ success: status, data: ret });
 });
 
-
-app.post('/update/:cropId', async (req, res) => {
+app.post("/update/:cropId", async (req, res) => {
   let [gotStatus, gotData] = await getCrop(req.params.cropId);
 
   if (!gotStatus) {
-    return res.send({"success": gotStatus, "data": gotData});
+    return res.send({ success: gotStatus, data: gotData });
   }
 
   let data = {
-    "$class": "org.orange.organicchain.Test",
-    "cropId": req.params.cropId,
-    "data": JSON.stringify(req.body)
+    $class: "org.orange.organicchain.Test",
+    cropId: req.params.cropId,
+    data: JSON.stringify(req.body),
   };
   console.log(data);
-  let [status, ret] = await axios.post(`${blockServer}/api/${entityName}`, data)
+  let [status, ret] = await axios
+    .post(`${blockServer}/api/${entityName}`, data)
     .then((resp) => {
       return [true, resp.data];
     })
@@ -82,14 +102,10 @@ app.post('/update/:cropId', async (req, res) => {
     });
   console.log(status, ret);
 
-  return res.send({"success": status, "data": ret});
+  return res.send({ success: status, data: ret });
 });
 
-
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
-);
-
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
