830e3036268cd524313e82e571941b7f6ae37f7d
Samyak S Sarnayak <samyak201@gmail.com>
2021-03-28 11:33:50 +0530

    api

--- functions/index.js
+++ functions/index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const frontendServer = "http://localhost";
+const frontendServer = "https://haxplore-orange.web.app";
 const blockServer = "http://34.87.52.243:3000";
 const entityName = "OrganicData";
 
